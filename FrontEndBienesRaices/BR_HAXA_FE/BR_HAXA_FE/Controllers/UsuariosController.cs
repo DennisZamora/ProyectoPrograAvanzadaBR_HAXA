d9bef3ce016c389a6208734a59a5c43339f622ff
Dennis Zamora Araya <denzamora2002@hotmail.com>
2022-04-17 12:50:39 -0600

    Controladores

---
+++ FrontEndBienesRaices/BR_HAXA_FE/BR_HAXA_FE/Controllers/UsuariosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BR_HAXA_FE.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
