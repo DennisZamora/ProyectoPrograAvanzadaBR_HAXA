8f9cff3c5c861606d67c12860b5a6ae5a7355e31
Dennis Zamora Araya <denzamora2002@hotmail.com>
2022-04-23 11:38:48 -0600

    Controller y layout

--- FrontEndBienesRaices/BR_HAXA_FE/BR_HAXA_FE/Controllers/RolsController.cs
+++ FrontEndBienesRaices/BR_HAXA_FE/BR_HAXA_FE/Controllers/RolsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BR_HAXA_FE.Models;
+using BR_HAXA_FE.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,137 @@ namespace BR_HAXA_FE.Controllers
 {
     public class RolsController : Controller
     {
-        public IActionResult Index()
+        private readonly IRolServices RolServices;
+
+        public RolsController(IRolServices rolServices)
+        {
+            RolServices = rolServices;
+        }
+
+        // GET: Rols
+        public async Task<IActionResult> Index()
+        {
+            return View(RolServices.GetAll());
+        }
+
+        // GET: Rols/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rol = RolServices.GetOneById((int)id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            return View(rol);
+        }
+
+        // GET: Rols/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: Rols/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdRol,NombreRol")] Rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                RolServices.Insert(rol);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rol);
+        }
+
+        // GET: Rols/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rol = RolServices.GetOneById((int)id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+            return View(rol);
+        }
+
+        // POST: Rols/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdRol,NombreRol")] Rol rol)
+        {
+            if (id != rol.IdRol)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    RolServices.Update(rol);
+                }
+                catch (Exception ex)
+                {
+                    if (!RolExists(rol.IdRol))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rol);
+        }
+
+        // GET: Rols/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rol = RolServices.GetOneById((int)id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            return View(rol);
+        }
+
+        // POST: Rols/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rol = RolServices.GetOneById((int)id);
+            RolServices.Delete(rol);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RolExists(int id)
+        {
+            return (RolServices.GetOneById((int)id) != null);
         }
     }
 }
